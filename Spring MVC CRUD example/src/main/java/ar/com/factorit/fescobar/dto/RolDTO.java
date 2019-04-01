a858b1ce54b960be3ab734db5767109e855ecfa3
smartywebdev0119 <smartwebdev0119@gmail.com>
2019-04-01 10:30:31 -0300

    Se agrega validación en backend

--- Spring MVC CRUD example/src/main/java/ar/com/factorit/fescobar/dto/RolDTO.java
+++ Spring MVC CRUD example/src/main/java/ar/com/factorit/fescobar/dto/RolDTO.java
@@ -2,6 +2,10 @@ package ar.com.factorit.fescobar.dto;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -9,11 +13,21 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class RolDTO {
 
 	private Integer id;
+	
+	@NotEmpty(message = "El nombre es requerido")
+	@Pattern(regexp = "^([a-zA-Z]+\\s)*[a-zA-Z]+$", message = "El nombre solo puede tener letras y espacios no consecutivos")
 	private String nombre;
+	
+	@NotEmpty(message = "La descripción es requerida")
+	@Pattern(regexp = "^([a-zA-Z]+\\s)*[a-zA-Z]+$", message = "La descripción solo puede tener letras y espacios no consecutivos")
 	private String descripcion;
+	
+	@NotNull(message = "La fecha de creación es requerida")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@JsonFormat(pattern = "yyyy-MM-dd")
 	private Date fechaCreacion;
+	
+	@NotNull(message = "El estado es requerido")
 	private boolean estado;
 
 	public Integer getId() {
